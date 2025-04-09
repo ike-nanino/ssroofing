cf765a286551727dd99cfbecc9150421295755e3
ike-nanino <ikesemanu@gmail.com>
2025-04-09 04:01:44 -0400

    added use client

--- app/second/page.tsx
+++ app/second/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Star, Phone, MapPin, Mail, ArrowRight, Check, Menu, X } from 'lucide-react';
